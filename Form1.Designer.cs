bc07e54f021b5234399f5c5b5ce12a0ea6d2becb
louis lee <jsjlyf@qq.com>
2020-01-11 11:51:42 +0800

    提交。

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -37,9 +37,9 @@
             this.label1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.label1.Location = new System.Drawing.Point(293, 139);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(55, 21);
+            this.label1.Size = new System.Drawing.Size(58, 21);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "你好！";
             // 
             // Form1
             // 
